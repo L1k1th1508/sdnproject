82e81978cb661b150577782977a520d1c55c5a08
L1k1th1508 <p.likith.adithya@gmail.com>
2026-04-25 15:24:43 +0530

    type of network setup

---
+++ topo.py
@@ -0,0 +1,20 @@
+from mininet.topo import Topo
+
+class MyStaticTopo(Topo):
+    def build(self):
+        # Add 3 switches
+        s1 = self.addSwitch('s1')
+        s2 = self.addSwitch('s2')
+        s3 = self.addSwitch('s3')
+        
+      
+        h1 = self.addHost('h1', ip='10.0.0.1/24')
+        h2 = self.addHost('h2', ip='10.0.0.2/24')
+
+       
+        self.addLink(h1, s1)
+        self.addLink(s1, s2)
+        self.addLink(s2, s3)
+        self.addLink(s3, h2)
+
+topos = { 'mystatictopo': (lambda: MyStaticTopo()) }
